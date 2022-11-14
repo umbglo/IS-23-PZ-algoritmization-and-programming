d8a89d44c1811eb50d183ac1b0e9a4ecebca6e3b
umbglo <113482977+umbglo@users.noreply.github.com>
2022-11-14 06:40:12 +0300

    Add files via upload

---
+++ PZ_code/PZ_4/PZ_4_1_code.py
@@ -0,0 +1,36 @@
+n, k = input('введите первое число: '), input('введите второе число: ')
+
+while type(n) != int: #обработка исключений
+    try:                #соблюдение положительности первого числа
+        n = int(n)
+    except ValueError:
+        print('введите целое число!')
+        n = input('введите первое число: ')
+    while n < 0:
+        try:
+            n = abs(n)
+        except TypeError:
+            print('неправильный ввод, попробуйте еще!')
+            n = input('введите первое число: ')
+    else:
+        n == n
+        
+while type(k) != int: #обработка исключений
+    try:                #соблюдение положительности первого числа
+        k = int(k)
+    except ValueError:
+        print('введите целое число!')
+        k = input('введите первое число: ')
+    while k < 0:
+        try:
+            k = abs(k)
+        except TypeError:
+            print('неправильный ввод, попробуйте еще!')
+            k = input('введите первое число: ')
+    else:
+        k == k
+
+result = 0
+for i in range(1, n + 1):
+    result += i ** k
+print(result)
